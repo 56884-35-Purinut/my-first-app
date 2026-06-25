3c36367a45cf9c707e3efbcd9e73b292c8b061e5
56884-35-Purinut <56884@yupparaj.ac.th>
2026-06-25 09:47:17 +0700

    Create digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import steamlit as at
+
+st.title("My Digital")
+
+st.header("แนะนำตัว")
+st.write("นายภูริณัฐ สุภาพ")
+st.write("ชื่อเล่น หมีภู")
+st.write("เลขที่ 19 ม.4/13")
+
+st.header("เป้าหมายที่อยากทำในอนาคต")
+st.write("เป้าหมาย : **อยากเรียนด้านคอมพิวเตอร์")
+st.write("สิ่งที่อยากเป็นอนาคต: นักอคสเกม (Game Caster)")
